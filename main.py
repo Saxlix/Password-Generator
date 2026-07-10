d6d42ec8bfb944f93a573e0f7fd66242459be9cf
Danya <35windows35@gmail.com>
2026-07-10 10:32:45 +0300

    создал класс БД и таблицу

--- main.py
+++ main.py
@@ -16,3 +16,17 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[logging.FileHandler("app.log", encoding="utf-8"), logging.StreamHandler(sys.stdout)]
 )
+
+class PhraseDatabase:
+    def __init__(self, db_path='passwords.db'):
+        self.db_path = db_path
+        self._init_db()
+
+    def _init_db(self):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute("PRAGMA journal_mode=WAL;")
+        cursor.execute('''CREATE TABLE IF NOT EXISTS generated_phrases (id INTEGER PRIMARY KEY AUTOINCREMENT, phrase TEXT NOT NULL, word_count INTEGER NOT NULL, separator TEXT NOT NULL, strength TEXT NOT NULL, date TEXT NOT NULL, image_path TEXT)''')
+        conn.commit()
+        conn.close()
+        logging.info("DB init success.")
